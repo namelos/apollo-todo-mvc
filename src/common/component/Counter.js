32e84d84a58619c1e43e98b247606f24c56ffe5c
Yifan Wang <namelos@outlook.com>
2016-11-19 14:44:17 +0800

    add deleteTodo mutation

--- src/common/component/Counter.js
+++ src/common/component/Counter.js
@@ -3,6 +3,33 @@ import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import { compose } from 'redux'
 
+const App = ({ data, addTodo, ...props }) => <div>
+  <p>Todos:</p>
+  <Todos todos={data.todos} {...props} />
+  <InputWithButton onClick={text => addTodo({ variables: {text} })}>
+    AddTodo
+  </InputWithButton>
+</div>
+
+const InputWithButton = ({ onClick, children }) => {
+  let text
+  return <div>
+    <input type="text" onChange={e => text = e.target.value}/>
+    <button onClick={_ => onClick(text)}>{children}</button>
+  </div>
+}
+
+const Todos = ({ todos, ...props }) => <ul>
+  { todos && todos.map((todo, i) => <li key={i}>
+    <Todo {...todo} {...props} />
+  </li>) }
+</ul>
+
+const Todo = ({ id, text, deleteTodo }) => <div>
+  <span>{id}: {text}</span>
+  <button onClick={_ => deleteTodo({ variables: {id} })}>x</button>
+</div>
+
 const query = gql`
   query {
     todos {
@@ -12,23 +39,7 @@ const query = gql`
   }
 `
 
-const App = ({ data, mutate }) => <div>
-  <p>Todos:</p>
-  <Todos todos={data.todos} />
-  <input type="text" onChange={ e => mutate({ variables: { text: e.target.value } }) }/>
-</div>
-
-const Todos = ({ todos }) => <ul>
-  { todos && todos.map((todo, i) => <li key={i}>
-    <Todo {...todo} />
-  </li>) }
-</ul>
-
-const Todo = ({ id, text }) => <p>
-  {id}: {text}
-</p>
-
-const add = gql`
+const addTodo = gql`
   mutation addTodo($text: String) {
     addTodo(text: $text) {
       id
@@ -37,12 +48,16 @@ const add = gql`
   }
 `
 
-// const addTodo = graphql(add, {
-//   name: 'addTodo'
-// })
+const deleteTodo = gql`
+  mutation deleteTodo($id: Int) {
+    deleteTodo(id: $id) {
+      text
+    }
+  }
+`
 
-export default
-  compose(
-    graphql(query),
-    graphql(add)
-  )(App)
+export default compose(
+  graphql(query),
+  graphql(addTodo, { name: 'addTodo' }),
+  graphql(deleteTodo, { name: 'deleteTodo' })
+)(App)
--- src/server/graphql.js
+++ src/server/graphql.js
@@ -1,7 +1,44 @@
 import { makeExecutableSchema } from 'graphql-tools'
-import typeDefs from './schema'
-import resolvers from './resolver'
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express'
+
+const typeDefs = `
+  type Todo {
+    id: Int
+    text: String
+  }
+  type Query {
+    todo(id: Int): Todo
+    todos: [Todo]
+  }
+  type Mutation {
+    addTodo(text: String): Todo
+    deleteTodo(id: Int): Todo
+  } 
+  schema {
+    query: Query
+    mutation: Mutation
+  }
+`
+
+const resolvers = (() => {
+  let todos = []
+
+  const genId = (_ => {
+    let id = 0
+    return _ => ++id
+  })()
+
+  return {
+    Query: {
+      todo:     (_, { id }) => todos.find(todo => todo.id === id),
+      todos:              _ => todos
+    },
+    Mutation: {
+      addTodo:  (_, {text}) => todos.push({id: genId(), text}),
+      deleteTodo: (_, {id}) => todos = todos.filter(todo => todo.id !== id)
+    }
+  }
+})()
 
 export const schema = makeExecutableSchema({ typeDefs, resolvers })
 export const graphql = graphqlExpress({ schema })
